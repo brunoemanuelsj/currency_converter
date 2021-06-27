c093a414cbea37961225271f485838e6a711e853
Bruno Jaime <emanuelbrunosj@gmail.com>
2021-06-27 19:37:14 -0300

    currency converter vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Conversor/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Conversor from "./components/Conversor.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Conversor
   }
 }
 </script>
 
 <style>
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: initial;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 1.2em;
+  font-weight: bold;
+  color: white;
 }
+
 </style>
